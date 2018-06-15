f44a0774e2aea102a78674eabce47bb2a18a783f
Mechasparrow <mikipux7@gmail.com>
2018-06-15 16:39:55 -0500

    added appropiate styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Import the Material Module
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
