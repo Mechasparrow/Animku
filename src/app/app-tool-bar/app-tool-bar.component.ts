f7e2ee5797492473731907d0785055059512bf83
Mechasparrow <mikipux7@gmail.com>
2018-06-16 23:25:36 -0500

    added simple tool bar

---
+++ src/app/app-tool-bar/app-tool-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tool-bar',
+  templateUrl: './app-tool-bar.component.html',
+  styleUrls: ['./app-tool-bar.component.css']
+})
+export class AppToolBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,28 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { WaifuCardoComponent } from './waifu-cardo/waifu-cardo.component';
+import { AppToolBarComponent } from './app-tool-bar/app-tool-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    WaifuCardoComponent
+    WaifuCardoComponent,
+    AppToolBarComponent
   ],
   imports: [
     BrowserModule,
     MatCardModule,
     MatButtonModule,
     MatGridListModule,
+    MatToolbarModule,
     FlexLayoutModule
   ],
   providers: [],
