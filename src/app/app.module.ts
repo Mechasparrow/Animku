c8b6c8458a64208c45ceff465ab54fc2f71acb6c
Mechasparrow <mikipux7@gmail.com>
2018-06-16 23:19:30 -0500

    added simple routing system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+//Required Angular Imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-//Import the Material Module
+//Import the Material Modules
 import { MatCardModule } from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
 
+//Root Angular Component
 import { AppComponent } from './app.component';
+
+//Layout system
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+//Pages for app
 import { HomePageComponent } from './home-page/home-page.component';
 
-import { FlexLayoutModule } from "@angular/flex-layout";
+//Developed Components for Application
 import { WaifuCardoComponent } from './waifu-cardo/waifu-cardo.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+//Routing config
+const appRoutes:Routes = [
+  {path: '', component: HomePageComponent}
+]
 
 @NgModule({
   declarations: [
@@ -23,7 +37,11 @@ import { WaifuCardoComponent } from './waifu-cardo/waifu-cardo.component';
     MatCardModule,
     MatButtonModule,
     MatGridListModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} //<-- For debugging only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
