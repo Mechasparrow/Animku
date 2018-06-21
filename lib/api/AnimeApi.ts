b9b22efb425c60e27371c406fbf266999d7fc2ae
Michael Navazhylau <mikipux7@gmail.com>
2018-06-21 10:33:40 -0500

    Anime preparal

--- lib/api/AnimeApi.ts
+++ lib/api/AnimeApi.ts
@@ -6,6 +6,9 @@ import {HttpClient} from '@angular/common/http';
 //RxJS stuff
 import { catchError, map, tap } from 'rxjs/operators';
 
+//Model
+import {Anime} from '../../models/Anime';
+
 @Injectable();
 
 //Interface responsible for getting us our animes
@@ -18,6 +21,13 @@ export class AnimeApi {
   private api_endpoint:string = "https://api.jikan.moe"
 
   constructor() {
+
+  }
+
+  //Get the animez from the search query
+  search(anime_name:string) {
+
+
 
   }
 
